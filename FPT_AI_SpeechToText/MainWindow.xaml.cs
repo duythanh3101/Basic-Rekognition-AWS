fb4f30677e14db127ea8d182e6dc19d898f6254c
duythanhphm <duythanh3101@gmail.com>
2020-05-30 15:50:14 +0700

    Text To Speech and Speech to Text using FPT AI api

---
+++ FPT_AI_SpeechToText/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FPT_AI_SpeechToText
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        class TTSData
+        {
+            public List<DictionaryEntry> hypotheses { get; set; }
+        }
+
+        class DictionaryEntry
+        {
+            public string utterance { get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtURL.Text != null)
+            {
+                string file = txtURL.Text.ToString();
+
+                if (!string.IsNullOrEmpty(file))
+                {
+                    var payload = File.ReadAllBytes(file);
+                    string result = Task.Run(async () =>
+                    {
+                        var client = new HttpClient();
+                        client.DefaultRequestHeaders.Add("api-key", "FEBGF6YdWXWxqGeq9oYJnArhHLr7aJZP");
+                        var response = await client.PostAsync("https://api.fpt.ai/hmi/asr/general", new ByteArrayContent(payload));
+                        return await response.Content.ReadAsStringAsync();
+                    }).GetAwaiter().GetResult();
+
+                    var data = JsonConvert.DeserializeObject<TTSData>(result);
+                    if (data != null && data.hypotheses != null && data.hypotheses.Count > 0)
+                    {
+                        resultTextBox.Text = data.hypotheses[0].utterance;
+                        MessageBox.Show(data.hypotheses[0].utterance);
+                    }
+                }
+
+            }
+
+        }
+
+        private void OpenDialog_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.mp3";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                if (openFileDialog.FileName != string.Empty)
+                {
+                    txtURL.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+    }
+}
---
+++ FPT_AI_TextToSpeech/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FPT_AI_TextToSpeech
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        class ReturnData
+        {
+            public string async { get; set; }
+            public string error { get; set; }
+            public string message { get; set; }
+            public string request_id { get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ContentTextBox.Text != null)
+            {
+                string payload = ContentTextBox.Text.ToString();
+                if (!string.IsNullOrEmpty(payload))
+                {
+                    string result = Task.Run(async () =>
+                    {
+
+                        var client = new HttpClient();
+                        client.DefaultRequestHeaders.Add("api-key", "FEBGF6YdWXWxqGeq9oYJnArhHLr7aJZP");
+                        client.DefaultRequestHeaders.Add("speed", "");
+                        client.DefaultRequestHeaders.Add("voice", "thuminh");
+                        var response = await client.PostAsync("https://api.fpt.ai/hmi/tts/v5", new StringContent(payload));
+                        return await response.Content.ReadAsStringAsync();
+                    }).GetAwaiter().GetResult();
+
+                    var data = JsonConvert.DeserializeObject<ReturnData>(result);
+                    var folder = AppDomain.CurrentDomain.BaseDirectory;
+                    var fileName = $"{folder}{Guid.NewGuid()}.mp3";
+
+
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadFile(data.async, fileName);
+                    }
+
+                    Thread.Sleep(2000); // wait 2 seconds to download successfully
+                    NotifyLabel.Content = "Download finished";
+
+                    var player = new MediaPlayer();
+                    player.Open(new Uri(fileName, UriKind.Absolute));
+
+                    NotifyLabel.Content = "Playing";
+                    player.MediaEnded += Player_MediaEnded;
+                    player.Play();
+                }
+                else
+                {
+                    NotifyLabel.Content = "Text is invalid";
+                }
+            }
+            
+        }
+
+        private void Player_MediaEnded(object sender, EventArgs e)
+        {
+            NotifyLabel.Content = "Play Finished";
+        }
+    }
+}
